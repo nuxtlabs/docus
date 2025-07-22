d55fe585a813e134a03e8c60aeb2eed28ac15df9
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-07-22 12:36:15 +0200

    up

--- cli/types.ts
+++ cli/types.ts
@@ -1,22 +1,7 @@
 export interface CLIOptions {
   name: string
   description: string
-  setup: DocsOptions
-}
-
-export interface DocsOptions {
-  defaults?: {
-    // Module name
-    name?: string
-    // Module description
-    description?: string
-    // Project directory
-    dir?: string
-    // Website URL
-    url?: string
-    // GitHub repository
-    github?: string
-    // GitHub branch
-    branch?: string
+  setup: {
+    defaults: Record<string, never>
   }
 }
