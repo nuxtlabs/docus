ec5d75e2c41fc4fef9255c453037ba3cddfad08f
Sébastien Chopin <atinux@gmail.com>
2025-09-10 18:58:17 +0200

    docs: test another analytics provider

--- docs/app/plugins/vercel.client.ts
+++ docs/app/plugins/vercel.client.ts
@@ -1,17 +1,19 @@
-// // import { onNuxtReady, defineNuxtPlugin, useNuxtApp } from '#imports'
-// import { inject, pageview } from '@vercel/analytics'
+import { inject, pageview } from '@vercel/analytics'
 
-// export default defineNuxtPlugin(() => {
-//   const nuxtApp = useNuxtApp()
+export default defineNuxtPlugin(() => {
+  const nuxtApp = useNuxtApp()
+  const route = useRoute()
 
-//   onNuxtReady(() => {
-//     inject({
-//       // basePath: ,
-//       disableAutoTrack: true,
-//       framework: 'nuxt',
-//     })
-//     nuxtApp.hooks.hook('page:finish', () => {
-//       // pageview(nuxtApp.payload.url)
-//     })
-//   })
-// })
+  onNuxtReady(() => {
+    inject({
+      disableAutoTrack: true,
+      framework: 'nuxt',
+    })
+    nuxtApp.hooks.hook('page:finish', () => {
+      pageview({
+        route: route.matched[0]?.path || route.path,
+        path: route.path,
+      })
+    })
+  })
+})
